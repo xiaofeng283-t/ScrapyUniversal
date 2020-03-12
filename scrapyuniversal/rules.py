cf849d9e6028fed9e585072511639d676fc0fb42
Jervon <jervon@foxmail.com>
2020-03-12 09:34:17 +0800

    fix: 修复爬取规则

--- scrapyuniversal/rules.py
+++ scrapyuniversal/rules.py
@@ -3,7 +3,7 @@ from scrapy.spiders import Rule
 
 rules = {
     'china': (
-        Rule(LinkExtractor(allow='article\/.*\.html', restrict_xpaths='//div[@id="left_side"]//div[@class="con_item"]'),
+        Rule(LinkExtractor(allow='article\/.*\.html', restrict_xpaths='//div[@id="rank-defList"]//div[@class="item_con"]'),
              callback='parse_item'),
         Rule(LinkExtractor(restrict_xpaths='//div[@id="pageStyle"]//a[contains(., "下一页")]'))
     )
